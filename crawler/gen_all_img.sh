e1cc0ab3323b9286fa09008103f0d7e034547f24
nigelnnk <nushsnigel@gmail.com>
2022-03-24 15:08:24 +0800

    Bash script + slight modifications on python script

---
+++ crawler/gen_all_img.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NUM_FILES=10
+conda activate reddit
+sleep 1
+
+echo "" > log.txt
+
+input="./subreddit_list.txt"
+while IFS= read -r line
+do
+  line=$(echo "$line" | tr '[:upper:]' '[:lower:]')
+  echo "$(date +%H:%M) Crawling from /r/$line"
+  python crawler.py -s "$line" -i "$NUM_FILES" >> log.txt
+  files=$(ls "$line/" | wc -l)
+  echo "$(date +%H:%M) Done getting $files/$NUM_FILES. Space: $(du -sh "$line/")"
+done < "$input"
+
+echo "All done!"
